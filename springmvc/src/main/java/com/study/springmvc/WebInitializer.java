afa662b5c283590da03b23d859d428934e449d04
renxiaochuan <15002742523@163.com>
2017-10-17 18:01:21 +0800

    mvc测试版

---
+++ springmvc/src/main/java/com/study/springmvc/WebInitializer.java
@@ -0,0 +1,24 @@
+package com.study.springmvc;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebInitializer implements WebApplicationInitializer {
+
+
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(MvcConfig.class);
+		ctx.setServletContext(servletContext);
+
+		Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet());
+		servlet.addMapping("/");
+		servlet.setLoadOnStartup(1);
+	}
+
+}
